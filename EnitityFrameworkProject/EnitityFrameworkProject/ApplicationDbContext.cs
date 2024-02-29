eb11e8ad32a1f5fa6eb46c466f413cc80de69339
SAIFUL ISLAM <saiful.islam888911@gmail.com>
2024-02-29 15:46:28 +0600

    Commit No.2 - Migration Generate & Apply-commit 1
    # Entity Added : Student
    # DbContext : ApplicationDbContext.cs
    # Migration : New migration added , Applied that migration to create table("CollegeStudents") in Database.

---
+++ EnitityFrameworkProject/EnitityFrameworkProject/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFrameworkProject
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public ApplicationDbContext()
+        {
+            //_connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;Database=Prac_CSharpB15;User Id=Practice_Project;Password=123456;Trust Server Certificate=True;";
+
+            _connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;" +
+                "Database=Prac_CSharpB15;" +
+                "User Id=Practice_Project;" +
+                "Password=123456;" +
+                "Trust Server Certificate=True;";
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /* [Note: * if the connection is not configured with DataBase,
+                        It will Congigure automatically.
+                      * "DbContextOptionsBuilder optionsBuilder" is used to configure "DbContext" ,
+                        options are provieded here to configure "DbContext". ]
+            */
+            if (optionsBuilder.IsConfigured == false)
+                optionsBuilder.UseSqlServer(_connectionString);
+
+
+            /* [Note: After overide in this method we also call the base classe's method ,
+                      base.OnCongiguring() will do rest of the work automatically.]
+            */
+            base.OnConfiguring(optionsBuilder);     
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+
+        public DbSet<Student> CollegeStudents { get; set; }
+    }
+}
